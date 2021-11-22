f1c9081e25ffe60b0350a63699111d8361c45a96
Prasad Ingole <bitgyro@gmail.com>
2021-11-22 21:53:29 +1100

    Initial commit for robofriends

---
+++ src/components/SearchBox.js
@@ -0,0 +1,8 @@
+const SearchBox = ({searchChange}) => {
+    return (
+        <input 
+        className = 'pa3 ba b--green bg-lightest-blue' type ="search" placeholder = 'search robots'
+        onChange={searchChange}></input>);
+  
+}
+export default SearchBox;
